799a5de277864ad9468f889c7da682a1299af8b0
fakhranii <ziad.fakhranyy@gmail.com>
2025-06-29 15:12:12 +0300

    succssed try via mobile phone

--- javascript/elsayed.js
+++
@@ -1,8 +0,0 @@
-let myArray = ["Ahmed", "Ail", "Zied", "Elsayed"];
-// myArray.unshift("Reda")
-let myObject = myArray.reduce((css, value, index) => {
-  css[index] = value;
-  return css;
-}, {});
-
-console.log(myObject);
